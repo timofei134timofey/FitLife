4d9ce90e03c987957e1784ed75d519442eea9b4f
Тим <8442988721@mail.ru>
2026-08-30 16:17:05 +0300

    Ревьюер замечания 1

--- fit_life.py
+++ fit_life.py
@@ -1,49 +1,54 @@
 """Спринт 1"""
 
 
+ML_IN_L = 1000
+ML_ON_KG = 30
+
+
 def check_age(qwery):
     """Вводим возраст"""
-    try:
-        user_age = int(input(qwery))
-        assert user_age > 0, 'Возраст должен быть больше нуля'
-    except ValueError:
-        print('Ошибка: Введите число')
-    except AssertionError as error:
-        print('Ошибка:', error)
-    else:
+    while True:
+        try:
+            user_age = int(input(qwery))
+        except ValueError:
+            print('Ошибка: Введите число')
+            continue
+        if user_age <= 0:
+            print('Возраст должен быть больше нуля')
+            continue
         return user_age
 
 
 def check_weight(qwery):
     """Вводим вес"""
-    try:
-        user_weight = float(input(qwery))
-        assert user_weight > 0, 'Вес должен быть больше нуля'
-    except ValueError:
-        print('Ошибка: Введите число')
-    except AssertionError as error:
-        print('Ошибка:', error)
-    else:
+    while True:
+        try:
+            user_weight = float(input(qwery))
+        except ValueError:
+            print('Ошибка: Введите число')
+            continue
+        if user_weight <= 0:
+            print('Вес должен быть больше нуля')
+            continue
         return user_weight
 
 
 def check_height(qwery, typs):
     """Вводим рост"""
-    try:
-        user_height = typs(input(qwery))
-        assert user_height > 0, 'Вес должен быть больше нуля'
-    except ValueError:
-        print('Ошибка: Введите число')
-    except AssertionError as error:
-        print('Ошибка:', error)
-    else:
+    while True:
+        try:
+            user_height = typs(input(qwery))
+        except ValueError:
+            print('Ошибка: Введите число')
+            continue
+        if user_height <= 0:
+            print('Рост должен быть больше нуля')
+            continue
         return user_height
 
 
 def calculation(user_weight, user_height):
     """Расчитываем остальное"""
-    ML_IN_L = 1000
-    ML_ON_KG = 30
     bmi: float = user_weight / (user_height ** 2)  # расчет индекса массы
     water_ml = user_weight * ML_ON_KG / ML_IN_L  # расчет нормы воды
     return bmi, water_ml
@@ -63,10 +68,11 @@ def main():
     """Главная"""
     user_name: str = input('Как Вас зовут? ')  # Вводим имя
     user_age = check_age('Сколько Вам лет? ')
-    user_weight: float = check_weight('Введите Ваш вес в кг ')
-    user_height: float = check_height('Введите Ваш рост вес в метрах ', float)
-    if user_name and user_age and user_weight and user_height:
-        show_message(user_name, user_age, user_weight, user_height)
+    user_weight: float = check_weight('Введите вес в кг ')
+    user_height: float = check_height(
+        'Введите рост в метрах, используя точку: 1.75 ', float
+    )
+    show_message(user_name, user_age, user_weight, user_height)
 
 
 if __name__ == '__main__':
